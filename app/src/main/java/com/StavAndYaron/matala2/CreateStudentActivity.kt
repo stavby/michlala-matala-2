e388ff0f40492ad5a7eaf237719073deefaba441
stavby <stav134679@gmail.com>
2025-01-04 21:03:22 +0200

    add create student form

--- app/src/main/java/com/StavAndYaron/matala2/CreateStudentActivity.kt
+++ app/src/main/java/com/StavAndYaron/matala2/CreateStudentActivity.kt
@@ -1,6 +1,5 @@
 package com.StavAndYaron.matala2
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -8,26 +7,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    private fun initAddStudentButton() {
-        val createStudentButton: Button = findViewById(R.id.main_activity_create_student_button)
-
-        createStudentButton.setOnClickListener {
-            val createStudentIntent = Intent(this, CreateStudentActivity::class.java)
-            startActivity(createStudentIntent)
+class CreateStudentActivity : AppCompatActivity() {
+    private fun initBackButton() {
+        val button: Button = findViewById(R.id.add_student_back_button)
+        button.setOnClickListener {
+            finish()
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add_student)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        initAddStudentButton()
+        initBackButton()
     }
 }
